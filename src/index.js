62a5df0f466f0aa9cfb1868a5cd50bb7357cfb3e
Plumbiu <3434909403@qq.com>
2023-09-12 23:01:20 +0800

    fix: github action options

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import fs, { readFileSync } from 'node:fs'
+import * as core from '@actions/core'
 import { svgHTML } from './canvas/index.js'
 
 const CLOUDLABEL = /<!-- cloud:start -->([\s\S]+)<!-- cloud:end -->/
 
-const words = ['hello', 'bar', 'foo', '你好', '世界']
-
 function run() {
   const md = readFileSync('README.md').toString()
+  if (!md) {
+    core.error('README.md is not exist')
+  }
   if (CLOUDLABEL.test(md)) {
+    const words = core.getInput('words')
+    core.info(words)
+    core.info('running~~~~')
     const label = CLOUDLABEL.exec(md)
     const input = label[0]
     const svg = input.replace(label[1], svgHTML(words))
     const newMd = md.replace(input, svg)
     fs.writeFileSync('./README.md', newMd)
+    core.info('successful!')
+    return
   }
+  core.info('some thing is error')
 }
 
 run()
