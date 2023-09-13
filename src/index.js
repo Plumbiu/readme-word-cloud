b7eb9622def22783e84b8a1caef37c006a5c3543
Plumbiu <3434909403@qq.com>
2023-09-13 10:07:47 +0800

    fix: no read README.md

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import fs, { readFileSync } from 'node:fs'
 import * as core from '@actions/core'
 import { svgHTML } from './canvas/index.js'
 
-const CLOUDLABEL = /<!-- cloud:start -->([\s\S]+)<!-- cloud:end -->/
 
 function run() {
-  const md = readFileSync('README.md').toString()
-  if (!md) {
-    core.error('README.md is not exist')
+  let words = core.getInput('words')
+  if (!words) {
+    words = ['not', 'word', 'in', 'your', 'config']
   }
-  if (CLOUDLABEL.test(md)) {
-    let words = core.getInput('words')
-    if (!words) {
-      words = ['not', 'word', 'in', 'your', 'config']
-    }
-    core.info('running~~~~')
-    svgHTML(words)
-    return
-  }
-  core.info('some thing is error')
+  svgHTML(words)
+  core.info('running~~~~')
 }
 
 run()
