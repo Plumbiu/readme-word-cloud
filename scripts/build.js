7d9d7d2409cd5dc844a551054e8d51caea8c50ae
Plumbiu <3434909403@qq.com>
2023-09-13 14:57:16 +0800

    fix: esbuild bug

---
+++ scripts/build.js
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild'
+await esbuild.build({
+  entryPoints: ['src/index.js'],
+  outdir: 'dist',
+  minify: true,
+  bundle: true,
+  treeShaking: true,
+  platform: 'node',
+  loader: {
+    '.node': 'file',
+  },
+  format: 'esm',
+  banner: {
+    js: `import { createRequire as topLevelCreateRequire } from 'module';
+    const require = topLevelCreateRequire(import.meta.url);`,
+  },
+})
